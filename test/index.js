43b5020a5a4487bf20ba7cefc06cfd9faa554dcc
Martin Heidegger <mh@ikkyotech.com>
2014-05-26 18:17:32 +0900

    Added fail redirect to hapi_passport implementation

--- test/index.js
+++ test/index.js
@@ -7,16 +7,39 @@ var hPass = require("../lib"),
     expect = Lab.expect;
 
 Lab.experiment("Make sure that all we implement all we know of passport", function () {
-    test("It should put redirect on the strategies object", function (done) {
+    test("It should pass down the authentication", function (done) {
+        var called = false,
+            myClass = function () { return undefined; };
+        myClass.prototype.authenticate = function () {
+            called = true;
+        };
+        hPass(myClass)(null, {});
+        expect(called).to.equal(true);
+        done();
+    });
 
+    test("It should put redirect on the strategies object", function (done) {
         var mockRedirect = function () { return undefined; },
-            called = false,
             myClass = function () { return undefined; };
         myClass.prototype.authenticate = function () {
             expect(this.redirect).to.equal(mockRedirect);
-            called = true;
         };
         hPass(myClass)(null, {redirect: mockRedirect});
+        done();
+    });
+
+    test("It should pass failure's to the failure option", function (done) {
+        var called = false,
+            mockFail = function (error) {
+                expect(error.message).to.equal("foo");
+                called = true;
+            },
+            myClass = function () { return undefined; };
+        myClass.prototype.authenticate = function () {
+            this.fail({message: "foo"});
+        };
+
+        hPass(myClass, { fail: mockFail })(null, {});
         expect(called).to.equal(true);
         done();
     });
