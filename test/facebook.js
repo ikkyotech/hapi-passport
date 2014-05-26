f7e1e8762698aec0929d2cc7b7a84b1c3f62a8ba
Martin Heidegger <mh@ikkyotech.com>
2014-05-26 17:30:09 +0900

    Added callback URL Test

--- test/facebook.js
+++ test/facebook.js
@@ -7,11 +7,27 @@ var Facebook = require("passport-facebook"),
     expect = Lab.expect;
 
 Lab.experiment("Making sure that the passport-facebook works as expected", function () {
+
     test("simplest request", function (done) {
         var redirectMock = nodemock.mock("redirect").takes("https://www.facebook.com/dialog/oauth?response_type=code&redirect_uri=&client_id=myClientId"),
             facebookImpl = new Facebook({
                 clientID: "myClientId",
                 clientSecret: "myClientSecret"
+            }, "http://callback");
+
+        facebookImpl.redirect = redirectMock.redirect;
+        facebookImpl.authenticate({}, {});
+        redirectMock.assert();
+        done();
+    });
+
+    test("Calling request", function (done) {
+        var callbackURL = "http://google.com?hello&world",
+            redirectMock = nodemock.mock("redirect").takes("https://www.facebook.com/dialog/oauth?response_type=code&redirect_uri=" + encodeURIComponent(callbackURL) + "&client_id=myClientId"),
+            facebookImpl = new Facebook({
+                clientID: "myClientId",
+                clientSecret: "myClientSecret",
+                callbackURL: callbackURL
             }, "http://callback");
 
         facebookImpl.redirect = redirectMock.redirect;
