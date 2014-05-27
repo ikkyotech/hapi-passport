089547d58c96f03ff820afc4fc8bc8443960689c
AtuyL <atuyl.jp@gmail.com>
2014-05-27 11:52:29 +0900

    added `userProfile` method test.

--- test/facebook.js
+++ test/facebook.js
@@ -66,32 +66,39 @@ Lab.experiment("Making sure that the passport-facebook works as expected", funct
             errorDescription = 'error desu' + errorToken,
             errorMock = nodemock.mock('error')
                 .takesF(function (error) {
-                    // console.log(error);
                     if( !(error instanceof AuthorizationError) ) return false;
                     if( error.uri !== errorUri) return false;
-                    // if(error.code === 'access_denied') return error.status === 403;
+                    if( error.message !== errorDescription) return false;
+                    if(error.code === 'access_denied') return error.status === 403;
                     if(error.code === 'server_error') return error.status === 502;
                     if(error.code === 'temporarily_unavailable') return error.status === 503;
                     // return error instanceof AuthorizationError;
                     return error.status === 500;
                 })
                 .times(7),
+            // Please see also http://tools.ietf.org/html/draft-ietf-oauth-v2-30#page-26
+            errorList = [
+                'invalid_scope', 'invalid_request', 'invalid_client',
+                'unauthorized_client', 'unsupported_response_type',
+                'temporarily_unavailable',
+                'server_error'
+            ],
             facebookImpl = new Facebook({
                 clientID: "myClientId",
                 clientSecret: "myClientSecret"
             }, "http://callback");
 
         facebookImpl.error = errorMock.error;
-
-        // Please see also http://tools.ietf.org/html/draft-ietf-oauth-v2-30#page-26
         // facebookImpl.authenticate({ query: { error: 'access_denied' } }, {});
-        facebookImpl.authenticate({ query: { error: 'invalid_scope', error_description:errorDescription, error_uri:errorUri } }, {});
-        facebookImpl.authenticate({ query: { error: 'invalid_request', error_description:errorDescription, error_uri:errorUri } }, {});
-        facebookImpl.authenticate({ query: { error: 'invalid_client', error_description:errorDescription, error_uri:errorUri } }, {});
-        facebookImpl.authenticate({ query: { error: 'unauthorized_client', error_description:errorDescription, error_uri:errorUri } }, {});
-        facebookImpl.authenticate({ query: { error: 'unsupported_response_type', error_description:errorDescription, error_uri:errorUri } }, {});
-        facebookImpl.authenticate({ query: { error: 'temporarily_unavailable', error_description:errorDescription, error_uri:errorUri } }, {});
-        facebookImpl.authenticate({ query: { error: 'server_error', error_description:errorDescription, error_uri:errorUri } }, {});
+        errorList.forEach(function (error) {
+            facebookImpl.authenticate({
+                query: {
+                    error: 'invalid_scope',
+                    error_description: errorDescription,
+                    error_uri: errorUri
+                }
+            }, {});
+        });
         
         errorMock.assert();
         done();
@@ -121,6 +128,130 @@ Lab.experiment("authorizationParams", function () {
                 expect(params.display).to.equal(options.display);
                 expect(params.auth_type).to.equal(options.authType);
                 expect(params.auth_nonce).to.equal(options.authNonce);
+            });
+        });
+        done();
+    });
+});
+
+Lab.experiment("userProfile", function () {
+    test("normal patterns", function (done) {
+        var InternalOAuthError = require('passport-oauth2').InternalOAuthError,
+            FacebookGraphAPIError = require('passport-facebook/lib/errors/facebookgraphapierror'),
+            
+            profileURL = 'http://www.facebook.com/AtuyL',
+            accessToken = 'dummy-access-token',
+            errorObjectList = [
+                "error: string",
+                {
+                    message:'error: json',
+                    type:'type',
+                    code:'code',
+                    error_subcode:'error_subcode'
+                },
+                null
+            ],
+            errorPatterns = [
+                errorObjectList[0], // InternalOAuthError
+                { data: JSON.stringify({ error: errorObjectList[1] }) }, // FacebookGraphAPIError
+                errorObjectList[2] // not error
+            ],
+            bodyObjectList = [
+                "{ unparsable json }",
+                {
+                    id: "id",
+                    username: "username",
+                    name: "name",
+                    last_name: "last_name",
+                    first_name: "first_name",
+                    middle_name: "middle_name",
+                    gender: "gender",
+                    link: "link",
+                    email: "email",
+                    picture: "picture"
+                },
+                {
+                    id: "id",
+                    username: "username",
+                    name: "name",
+                    last_name: "last_name",
+                    first_name: "first_name",
+                    middle_name: "middle_name",
+                    gender: "gender",
+                    link: "link",
+                    email: "email",
+                    picture: {
+                        data: {
+                            url: "picture_data_url"
+                        }
+                    }
+                }
+            ],
+            bodyPatterns = [
+                bodyObjectList[0],
+                JSON.stringify(bodyObjectList[1]),
+                JSON.stringify(bodyObjectList[2])
+            ],
+            facebookImpl = new Facebook({
+                clientID: "800464516633425",
+                clientSecret: "b78c7b021c42acb72242f1d7e354264a",
+                callbackURL: "http://localhost:8080/callback",
+                profileURL: profileURL
+            }, "http://callback");
+
+        facebookImpl.redirect = function (authpath) {
+            console.log(authpath);
+        };
+
+        errorPatterns.forEach(function (error, eIndex) {
+            bodyPatterns.forEach(function (body, bIndex) {
+                // console.log('\n--------', 'error:', eIndex, 'body:', bIndex);
+                // console.log(error);
+                // console.log(body);
+
+                facebookImpl._oauth2 = nodemock.mock('get')
+                    .takes( profileURL, accessToken, function (){} )
+                    .calls(2, [error, body, {}]);
+                facebookImpl.userProfile(accessToken, function (e, p) {
+                    if (e) {
+                        if (e instanceof InternalOAuthError){
+                            expect(p).to.equal(undefined);
+                            expect(e.name).to.equal('InternalOAuthError');
+                            expect(e.message).to.equal('Failed to fetch user profile');
+                            expect(e.oauthError).to.equal(errorObjectList[0]);
+                        } else if (e instanceof FacebookGraphAPIError) {
+                            expect(p).to.equal(undefined);
+                            expect(e.name).to.equal('FacebookGraphAPIError');
+                            expect(e.message).to.equal(errorObjectList[1].message);
+                            expect(e.type).to.equal(errorObjectList[1].type);
+                            expect(e.code).to.equal(errorObjectList[1].code);
+                            expect(e.subcode).to.equal(errorObjectList[1].error_subcode);
+                            expect(e.status).to.equal(500);
+                        } else if (e instanceof Error) {
+                            expect(e.message).to.equal('Failed to parse user profile');
+                        } else {
+                            done(new Error('Unexpected Error Type.'));
+                        }
+                    }else{
+                        expect(e).to.equal(null);
+                        var ref = bodyObjectList[bIndex];
+                        expect(p.id).to.equal(ref.id);
+                        expect(p.username).to.equal(ref.username);
+                        expect(p.displayName).to.equal(ref.name);
+                        expect(p.name.familyName).to.equal(ref.last_name);
+                        expect(p.name.givenName).to.equal(ref.first_name);
+                        expect(p.name.middleName).to.equal(ref.middle_name);
+                        expect(p.gender).to.equal(ref.gender);
+                        expect(p.profileUrl).to.equal(ref.link);
+                        expect(p.emails[0].value).to.equal(ref.email);
+                        if(typeof ref.picture === 'string'){
+                            expect(p.photos[0].value).to.equal(ref.picture);
+                        }else{
+                            expect(p.photos[0].value).to.equal(ref.picture.data.url);
+                        }
+                        expect(p.provider).to.equal('facebook');
+                    }
+                });
             });
         });
         done();
